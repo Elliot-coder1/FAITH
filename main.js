0c95bd55da767440fb9f2e5faabb64143e105e1b
Elliot <obukoyibooghenefejiro@gmail.com>
2025-03-09 12:05:30 +0100

    play

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function showSidebar(){
   const sidebar = document.querySelector('.sidebar');
-  sidebar.style.display = 'flex';
+  sidebar.style.display = 'block';
 }
 function hideSidebar(){
   const sidebar = document.querySelector('.sidebar');
