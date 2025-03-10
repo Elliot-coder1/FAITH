93a2cd7b8b95860a7f1e0fe37c0fe3dcd8d3c464
Elliot <obukoyibooghenefejiro@gmail.com>
2025-03-10 15:43:16 +0100

    fix

--- main.js
+++ main.js
@@ -131,26 +131,6 @@ animateAboutSlideIn();
 
 
 
-//new styles
-// let lightmode = localStorage.getItem('lightmode')
-//const themeSwitch = document.getElementById('theme-switch')
-
-//const enableLightmode = () => {
-  //document.body.classList.add('lightmode')
- // localStorage.setItem('lightmode', 'active')
-//}
-
-//const disableLightmode = () => {
- // document.body.classList.remove('lightmode')
-  //localStorage.setItem('lightmode', null)
-//}
-
-//if(lightmode === "active") enableLightmode()
-
-//themeSwitch.addEventListener("click", () => {
- // lightmode = localStorage.getItem('lightmode')
-  //lightmode !== "active" ? enableLightmode() : disableLightmode()
-//});
 
 
 
@@ -162,5 +142,5 @@ icon.onclick = function(){
     icon.src ="sun.png"
   }else{
     icon.src = "moon.png"
-  }
-}
+  };
+};
